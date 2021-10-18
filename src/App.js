0c855c4dc527399751bb1c462c0e69482e687127
GabrielJorge <gabrieljorge.ca@gmail.com>
2021-10-18 19:03:34 -0400

    add link to pokemon id

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
 import pokemons from './data';
+import Pokemon from './Pokemon'
 import Pokedex from './Pokedex';
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
       <BrowserRouter> 
+      <Link to='/pokemons/:id'>More info</Link>
       <Switch>
-      <Route path="/" render={(props) => <Pokedex {...props} pokemons={pokemons} />}  />
+      <Route path="/" render={(props) => <Pokedex {...props} pokemons={pokemons} />} />
       </Switch>
       </BrowserRouter>
     </div>
